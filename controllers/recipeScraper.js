491db3282d67a318828a4eb07fb736428968e5ae
CHARLEY CARRIERO <carriero.char@gmail.com>
2021-06-11 19:14:56 -0400

    feat: implement data extraction from json and send back to client

--- controllers/recipeScraper.js
+++ controllers/recipeScraper.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 const _ = require('lodash');
+const { json } = require('express');
 
 
 const fetchWithTimeout = (url, options, timeout = 5000) => {
@@ -17,11 +18,9 @@ const fetchHtml = (url) => {
 }
 
 const parseHtml = (html) => {
-
   const $ = cheerio.load(html);
   const jsonld = $('script[type="application/ld+json"]').html();
   if (!jsonld) return false;
-
   const recipe = JSON.parse(jsonld);
 
   let nestedRecipe = {};
@@ -45,15 +44,57 @@ const parseHtml = (html) => {
   }
 }
 
+const extractData = (jsonld) => {
+  desiredKeys = ['name','keywords','recipeYield', 'recipeIngredient','image', 'recipeInstructions', 'publisher', 'author']
+  const recipe = {}
+
+  for (key of desiredKeys) {
+    if (jsonld.hasOwnProperty(key)) {
+
+      if (key === 'keywords' && typeof jsonld[key] === 'string') {
+        recipe[key] = jsonld[key].split(',');
+
+      } else if (key === 'image' && Array.isArray(jsonld[key])) {
+        recipe[key] = jsonld[key][0];
+
+      } else if (key === 'recipeYield' && typeof jsonld[key] !== 'string') {
+        recipe[key] = '';
+
+      } else if (key === 'recipeInstructions') {
+        recipe[key] = jsonld[key].map(obj => obj.text);
+
+      } else if (key === 'publisher') {
+        if (jsonld[key].hasOwnProperty('name')) recipe[key] = jsonld[key].name;
+
+      } else if (key === 'author') {
+        if (Array.isArray(jsonld[key])) {
+          recipe[key] = jsonld[key].map(obj => obj.name).join(',');
+        } else if (jsonld[key].hasOwnProperty('name')) {
+          recipe[key] = jsonld[key].name;
+        }
+
+      } else {
+        recipe[key] = jsonld[key]
+      }
+    }
+  }
+
+  return recipe;
+}
+
+
 const handleScrape = async (req, res) => {
   try {
     const html = await fetchHtml(req.body.url);
     const jsonld = parseHtml(html);
     if(!jsonld) throw new Error('no json ld');
 
-    res.status(200).json(jsonld);
+    const recipe = extractData(jsonld);
+    recipe.url = req.body.url
+    res.status(200).json(recipe);
 
   } catch (e) {
+    console.log(e);
     res.status(400).send(e.message);
   }
 
