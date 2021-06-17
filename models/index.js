532692a6f0ba5195614f4c10002c7a0223814716
Ujwal Kandukuri <ujwal.kanduk@gmail.com>
2021-06-17 11:14:01 -0400

    understanding git flow

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const { DB_CONN } = process.env;
 
 mongoose.connect(
-   DB_CONN,
+  DB_CONN,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err) => {
     if (err) {
