e60e712d225e7f45729b8f1590de01635a52feaf
Santiago Vásquez Yacamán <santiagoyacaman@uninorte.edu.co>
2021-06-17 16:27:50 -0500

    feat: compelted ts in recipeScraper.ts

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const userController = require('./controllers/users');
-const recipeScrapingController = require('./controllers/recipeScraper');
+const recipeScrapingController = require('./controllers/recipeScraper.ts');
 const recipeController = require('./controllers/recipe.ts');
 const authMiddleware = require('./middlewares/auth');
 
